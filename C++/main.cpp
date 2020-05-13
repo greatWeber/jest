83f04fc4096ef2ac6825f3ba0294ed8a0c12f509
xlf <my_hero_IronMan@outlook.com>
2020-05-13 20:25:44 +0800

    修改

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 using namespace std;
+
+//  函数定义
+int add(int a, int b)
+{
+    return a+b;
+}
 int main()
 {
     cout <<"hello world !!"<< endl;
@@ -7,5 +13,13 @@ int main()
     typedef int Int;
     Int b = 123;
     cout << b << endl;
+    // 枚举
+    enum Color {red,green,blue} color;
+    color = green;
+    cout << color << endl;
+
+    int c = add(1, 2);
+    cout << c << endl;
     return 0;
 }
+
