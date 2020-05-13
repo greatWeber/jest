f49ca7ebf8799a779335dbe4389cf4fe1b343322
xlf <my_hero_IronMan@outlook.com>
2020-05-13 19:19:28 +0800

    添加c++练习

---
+++ C++/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout <<"hello world !!"<< endl;
+    // typedef 声明
+    typedef int Int;
+    Int b = 123;
+    cout << b << endl;
+    return 0;
+}
