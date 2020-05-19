3313fc478a46c8417769f10cd70870de9c0eb827
xlf <my_hero_IronMan@outlook.com>
2020-05-19 10:17:46 +0800

    前缀树案例3

---
+++ src/leetcode/implementTriePrefixTree-2.js
@@ -0,0 +1,92 @@
+//  前缀树
+
+/**
+ * 给定一个二维网格 board 和一个字典中的单词列表 words，找出所有同时在二维网格和字典中出现的单词。
+
+单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母在一个单词中不允许被重复使用。
+
+示例:
+
+输入:
+words = ["oath","pea","eat","rain"] and board =
+[
+  ['o','a','a','n'],
+  ['e','t','a','e'],
+  ['i','h','k','r'],
+  ['i','f','l','v']
+]
+
+输出: ["eat","oath"]
+说明:
+你可以假设所有输入都由小写字母 a-z 组成。
+ */
+
+ function TrieNode(val){
+     this.val = val;
+     this.children = {};
+     this.isWorld = false; // 是否在尾部
+ }
+
+ function Trie (){
+     this.root = new TrieNode(null);
+ }
+
+ Trie.prototype.addWord = function(word){
+     let current = this.root;
+     for(let i in word){
+         let c = word[i];
+         if(!current.children[c]){
+            current.children[c] = new TrieNode(c);
+         }
+         current = current.children[c];
+     }
+     current.isWorld = true;
+ }
+
+ Trie.prototype.startsWith = function(word){
+    let current = this.root;
+    for(let i in word){
+        let c = word[i];
+        if(!current.children[c]) return false;
+        current = current.children[c];
+    }
+    return true;
+}
+
+export const findWords = (boards,words)=>{
+    if(boards.length == 0) return [];
+    const trie = new Trie();
+    const map = new Map();
+    const rs = [];
+    const m = boards.length;
+    const n = boards[0].length; // 二维数组的长度
+    for(let word of words){
+        trie.addWord(word);
+    }
+
+    const dfs = (str,row,col)=>{
+        if(row<0||row>=m||col<0||col>=n||!trie.startsWith(str)) return;
+
+        str+= boards[row][col];
+        if(words.includes(str)){
+            rs.push(str);
+        }
+        dfs(str,row+1,col);
+        dfs(str,row-1,col);
+        dfs(str,row,col+1);
+        dfs(str,row,col-1);
+    }
+
+    for(let i=0;i<m;i++){
+        for(let k=0;k<n;k++){
+            dfs('',i,k);
+        }
+    }
+
+    return rs;
+}
+
+ export default {
+    Trie,
+    findWords
+ }
--- tests/unit/implementTriePrefixTree.spec.js
+++ tests/unit/implementTriePrefixTree.spec.js
@@ -1,8 +1,10 @@
 import implementTriePrefixTree from '@/leetcode/implementTriePrefixTree';
 import implementTriePrefixTree1 from '@/leetcode/implementTriePrefixTree-1';
+import implementTriePrefixTree2 from '@/leetcode/implementTriePrefixTree-2';
 
 const Trie = implementTriePrefixTree.Trie;
 const Trie1 = implementTriePrefixTree1.Trie;
+const findWords = implementTriePrefixTree2.findWords;
 
 describe('前缀树',()=>{
     test('前缀树-1',()=>{
@@ -13,7 +15,7 @@ describe('前缀树',()=>{
         ;
     });
 
-    test.only('前缀树-2',()=>{
+    test('前缀树-2',()=>{
         let trie = new Trie1();
         trie.addWord('app');
         trie.addWord('bar');
@@ -24,5 +26,18 @@ describe('前缀树',()=>{
         expect(trie.search('b..')).toEqual(true);
         expect(trie.search('b.d')).toEqual(true);
         expect(trie.search('b..')).toEqual(true);
+    });
+
+    test.only('前缀树-3',()=>{
+        const words = ["oath","pea","eat","rain"];
+        const boards = [
+            ['o','a','a','n'],
+            ['e','t','a','e'],
+            ['i','h','k','r'],
+            ['i','f','l','v']
+          ];
+
+          const rs = findWords(boards,words);
+          console.log(rs);
     })
 })
